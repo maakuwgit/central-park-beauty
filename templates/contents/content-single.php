dc6b055df2706585e3bce77af2107c2e52651be8
MarkW <mark@liftedlogic.com>
2018-08-01 17:03:13 -0500

    Blog Single WIP. Related articles needed to be pulled from the ACFs and styled

--- templates/contents/content-single.php
+++ templates/contents/content-single.php
@@ -1,25 +1,39 @@
+<?php
+  $bg_img = get_post_thumbnail_id();
+
+  if( $bg_img ) {
+    $bg_img_full = wp_get_attachment_image_url($bg_img, 'full');
+    $bg_img_lg = wp_get_attachment_image_url($bg_img, 'large');
+    $bg_img_md = wp_get_attachment_image_url($bg_img, 'medium');
+  }
+
+?>
 <article <?php post_class(); ?>>
 
   <div class="post container">
 
-    <div class="row start">
+      <header class="post__header relative flex row start"<?php if($bg_img) echo ' data-backgrounder';?>>
 
-      <header class="post__header col col-10of12 white">
+      <?php if ($bg_img) : ?>
+        <div class="feature">
+          <img alt="" src="<?php echo $bg_img_md; ?>" srcset="<?php echo $bg_img_lg; ?> 2x, <?php echo $bg_img_full; ?> 3x" data-src-md="<?php echo $bg_img_md; ?>" data-src-lg="'<?php echo $bg_img_lg; ?>" data-src-xl="<?php echo $bg_img_full; ?>">
+        </div>
+      <?php endif; ?>
 
         <svg class="post__headline icon icon-BLOG">
           <use xlink:href="#icon-BLOG"></use>
         </svg>
         <!-- .post__headline icon icon-BLOG -->
 
-        <time class="published post__supertitle" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
-        <!-- . -->
-        <h1 class="post__title"><?php the_title(); ?></h1>
-        <!-- . -->
+        <div class="col col-md-9of12 col-lg-9of12 col-xl-9of12 white">
+          <time class="published post__header__supertitle" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
+          <!-- . -->
+          <h1 class="post__header__title"><?php the_title(); ?></h1>
+          <!-- . -->
+        </div>
 
       </header>
-      <!-- .post__header col col-10of12 white -->
-
-    </div>
+      <!-- .post__header.row.start -->
 
     <div class="row centered">
 
@@ -28,6 +42,7 @@
       </div>
 
       <div class="post__related_posts col col-md-3of12 col-lg-2of12 col-xl-2of12">
+        Related News
       </div>
 
     </div>
