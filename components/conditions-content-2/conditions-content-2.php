078a8384f1ae6707ae85eae01e4638358585c762
MarkW <mark@liftedlogic.com>
2018-08-01 11:38:57 -0500

    Cleanup

--- components/conditions-content-2/conditions-content-2.php
+++ components/conditions-content-2/conditions-content-2.php
@@ -59,29 +59,34 @@ if ($background ){
 <?php if ( ll_empty( $component_data ) ) return; ?>
 <div class="cp-conditions-content-2<?php echo $bg . implode( " ", $classes ); ?>" <?php echo ( $component_id ? 'id="'.$component_id.'"' : '' ) ?> data-component="conditions-content-2">
 
-  <div class="container">
+  <div class="container row">
 
-    <div class="row">
+    <?php if( $left_content ) : ?>
+    <div class="col col-lg-6of12 col-xl-6of12">
+      <span class="cp-conditions-content-2__left-heading<?php echo $color; ?>"><?php echo $title; ?></span>
 
-      <?php if( $left_content ) : ?>
-      <div class="col col-lg-6of12 col-xl-6of12">
-        <span class="cp-conditions-content-2__left-heading<?php echo $color; ?>"><?php echo $title; ?></span>
-        <div class="cp-conditions-content-2__left-text">
-          <?php echo $left_text; ?>
-        </div>
+      <div class="cp-conditions-content-2__left-text">
+        <?php echo $left_text; ?>
       </div>
-      <?php endif; ?>
-
-      <?php if( $right_content ) : ?>
-      <div class="col col-lg-6of12 col-xl-6of12">
-        <div class="cp-conditions-content-2__right-text">
-          <?php echo $right_content; ?>
-        </div>
-      </div>
-      <?php endif; ?>
+      <!-- .cp-conditions-content-2__left-text -->
 
     </div>
+    <!-- .col.col-lg-6of12.col-xl-6of12 -->
+    <?php endif; ?>
+
+    <?php if( $right_content ) : ?>
+    <div class="col col-lg-6of12 col-xl-6of12">
+
+      <div class="cp-conditions-content-2__right-text">
+        <?php echo $right_content; ?>
+      </div>
+      <!-- .cp-conditions-content-2__right-text -->
+
+    </div>
+    <!-- .col col-lg-6of12 col-xl-6of12 -->
+    <?php endif; ?>
 
   </div>
+  <!-- .container -->
 
 </div>
