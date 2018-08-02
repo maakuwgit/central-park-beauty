5f5a96d2d952cdd5eaa6086117a50334bfef5c5f
MarkW <mark@liftedlogic.com>
2018-08-02 13:35:41 -0500

    WIP Events Archive. Still need the cost

---
+++ templates/contents/content-single-event.php
@@ -0,0 +1,54 @@
+<?php $image = get_the_post_thumbnail(); ?>
+<div class="card__wrapper col col-sm-6of12 col-md-4of12 col-lg-4of12 col-xl-4of12">
+
+  <div class="card-grid__card" data-clickthrough>
+
+  <?php if( $image ) : ?>
+    <figure class="card-grid__feature__wrapper" data-backgrounder>
+
+      <div class="card-grid__feature feature">
+
+      <?php echo $image; ?>
+
+      </div><!-- .card-grid__feature.feature -->
+
+    </figure>
+  <?php else: ?>
+
+    <figure class="card-grid__feature__wrapper">
+
+      <div class="card-grid__feature feature"></div><!-- .card-grid__feature.feature -->
+
+    </figure>
+  <?php endif; ?>
+
+  <div class="card-grid__body">
+    <?php
+      $treatments = get_the_terms( get_the_ID(), 'service_category');
+
+      if ($treatments) :
+
+        foreach($treatments as $treatment) :
+      ?>
+
+        <span class="card-grid__meta"><?php echo $treatment->name; ?></span>
+        <!-- .card-grid__meta -->
+
+          <?php
+        endforeach;
+
+      endif;
+      ?>
+      <time class="card-grid__meta published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
+
+      <h3 class="card-grid__title">
+        <a href="<?php the_permalink(); ?>"><?php echo the_title(); ?></a>
+      </h3>
+      <!-- .card-grid__title -->
+    </div>
+
+    <?php get_template_part('templates/partials/event-meta'); ?>
+
+  </div>
+
+</div><!-- .card-grid -->
---
+++ templates/partials/event-meta.php
@@ -0,0 +1,4 @@
+<div class="event__meta row between">
+  <time class="event__duration" datetime=""><?php echo '6pm-9pm'; ?></time>
+  <span class="learn_more">Learn more</span>
+</div>
