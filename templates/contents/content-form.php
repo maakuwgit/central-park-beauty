4696bb124f056c2a0278cb8b16a093af5c9245b4
MarkW <mark@liftedlogic.com>
2018-08-02 12:27:20 -0500

    Contact Us WIP Header Image Responsive work

--- templates/contents/content-form.php
+++ templates/contents/content-form.php
@@ -1,15 +1,59 @@
 <?php
   $form_id = ( get_field('form_id') ? get_field('form_id') : 1 );
+
+  $bg_img = get_post_thumbnail_id();
+
+  if( $bg_img ) {
+    $bg_img_full = wp_get_attachment_image_url($bg_img, 'full');
+    $bg_img_lg = wp_get_attachment_image_url($bg_img, 'large');
+    $bg_img_md = wp_get_attachment_image_url($bg_img, 'medium');
+  }
+
 ?>
 <article <?php post_class(); ?>>
 
   <div class="container-full">
-<?php if( is_plugin_active( 'gravityforms/gravityforms.php' ) ) : ?>
 
-  <?php gravity_form( $form_id, true, true ); ?>
-  <!-- .form-skin -->
+    <div class="row stretch start">
 
-<?php endif; ?>
+  <?php if( is_plugin_active( 'gravityforms/gravityforms.php' ) && $bg_img ) : ?>
+      <figure class="form-template__feature col col-md-6of12 col-lg-6of12 col-xl-6of12" data-backgrounder>
+
+        <div class="feature">
+          <img alt="" src="<?php echo $bg_img_md; ?>" srcset="<?php echo $bg_img_lg; ?> 2x, <?php echo $bg_img_full; ?> 3x" data-src-md="<?php echo $bg_img_md; ?>" data-src-lg="'<?php echo $bg_img_lg; ?>" data-src-xl="<?php echo $bg_img_full; ?>">
+        </div>
+
+      </figure>
+
+      <div class="col col-md-5of12 col-lg-5of12 col-xl-5of12">
+
+        <?php gravity_form( $form_id, true, true ); ?>
+        <!-- .form-skin -->
+
+      </div>
+      <!-- .col.col-6of12 -->
+  <?php elseif( $bg_img ): ?>
+
+    <figure class="col" data-backgrounder>
+
+        <div class="feature">
+          <img alt="" src="<?php echo $bg_img_md; ?>" srcset="<?php echo $bg_img_lg; ?> 2x, <?php echo $bg_img_full; ?> 3x" data-src-md="<?php echo $bg_img_md; ?>" data-src-lg="'<?php echo $bg_img_lg; ?>" data-src-xl="<?php echo $bg_img_full; ?>">
+        </div>
+
+      </figure>
+
+  <?php elseif( is_plugin_active( 'gravityforms/gravityforms.php' ) ) : ?>
+
+      <div class="col">
+
+        <?php gravity_form( $form_id, true, true ); ?>
+        <!-- .form-skin -->
+
+      </div>
+
+  <?php endif; ?>
+
+    </div>
 
 <?php
   $map = array(
@@ -24,6 +68,6 @@
   );
 ?>
 </div>
-<!-- .container -->
+<!-- .container-full -->
 
 </article>
